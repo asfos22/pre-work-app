7fb3060913ec5afd853b092c5368bfffd470aa9d
unknown <ridamsaxena90@gmail.com>
2019-08-30 20:20:24 +0530

    Updates

---
+++ precache-manifest.9348841581a30570e0674e761e935acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d817aae078c0e87fc5cea5c5a8bd7bee",
+    "url": "/Ironpurush0/pre-work-app.git/index.html"
+  },
+  {
+    "revision": "1ad105b8c902da494593",
+    "url": "/Ironpurush0/pre-work-app.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "79ce8d7a7c1b1e4ada80",
+    "url": "/Ironpurush0/pre-work-app.git/static/js/2.0ceb4700.chunk.js"
+  },
+  {
+    "revision": "1ad105b8c902da494593",
+    "url": "/Ironpurush0/pre-work-app.git/static/js/main.4d37eb3e.chunk.js"
+  },
+  {
+    "revision": "2cd2f4cea516d60e6415",
+    "url": "/Ironpurush0/pre-work-app.git/static/js/runtime~main.5ba89f70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ironpurush0/pre-work-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
